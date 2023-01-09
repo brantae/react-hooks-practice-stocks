e1a9308ddb033f173d86bc29c67b85dd20362cb5
Anslie Brant <brantae@gmail.com>
2023-01-09 11:31:11 -0600

    project half done

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+
+  const [stocks, setStocks] = useState([])
+  const [myStocks, setMyStocks] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+    .then((r) => r.json())
+    .then((data) => {
+      setStocks(data)
+
+    })
+  }, [])
+
+  function buyStock(stock) {
+    if (!myStocks.includes(stock)) {
+      const updatedMyStocks = [...myStocks, stock]
+      setMyStocks(updatedMyStocks)
+    }else{
+      alert('You already bought it!')
+    }
+  }
+
+  function sellStock(stock) {
+    const updatedMyStocks = [...myStocks].filter(myStock => myStock.id !== stock.id)
+    setMyStocks(updatedMyStocks)
+  }
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} handleClick={buyStock} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer stocks={myStocks} handleClick={sellStock}/>
         </div>
       </div>
     </div>
